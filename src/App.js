01e35c0202706b2d125b2483049b091f9f90ad64
Toni Soler <tonisoler1986@gmail.com>
2023-06-12 09:55:10 +0200

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import "./App.css";
+import React, { useState } from "react";
+import contacts from "./contacts.json";
 
 function App() {
+  const contactsArrayFive = contacts.splice(0, 5);
+
+  const [contactsArray, setContactsArray] = useState(contactsArrayFive);
+  const remainingContacts = contacts;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <h1>IRON CONTACTS</h1>
+
+<button type="button">Add Random Contact</button>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy </th>
+        </tr>
+        {contactsArray.map((element) => {
+        return <tr>
+          <td>{element.name}</td>
+          <td><img src= {element.pictureUrl} alt = "foto"/></td>
+          <td>{element.popularity}</td>
+          <td>
+                  {element.wonOscar ? (
+                    <span role="img" aria-label="Oscar Trophy">
+                      🏆
+                    </span>
+                  ) : null}
+                </td>
+                <td>
+                  {element.wonEmmy ? (
+                    <span role="img" aria-label="Emmy Trophy">
+                      🏆
+                    </span>
+                  ) : null}
+                </td>
+        </tr>
+      })}
+        
+      </table>
+  
     </div>
   );
 }
-
 export default App;
